5d6d5aa6ea285288c3077ea424bc1ac09f950ee5
Jonathan-Ultini <jonathanultini@gmail.com>
2024-09-03 15:38:35 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Script di base per il progetto
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Il DOM è completamente caricato e analizzato');
+
+  // Aggiungi qui il tuo codice JavaScript
+});
