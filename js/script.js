550c6f055bceb6ee9cf6e53ba37c55ff17e1a6b2
Jonathan-Ultini <jonathanultini@gmail.com>
2024-09-03 15:39:36 +0200

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 // Script di base per il progetto
 
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('Il DOM è completamente caricato e analizzato');
 
-  // Aggiungi qui il tuo codice JavaScript
+const app = Vue.createApp({
+  data() {
+    return {
+      message: 'Benvenuto nel mio Progetto Web con Vue.js!',
+      imageSrc: 'https://media.istockphoto.com/id/1482199015/it/foto/felice-cucciolo-welsh-corgi-14-settimane-cane-ammiccante-ansimante-e-seduto-isolato-su-bianco.jpg?s=612x612&w=0&k=20&c=oveIBmaFDoOHa84pH4odLQaAs0-C7hXckvWanqFXqYs=' // Sostituisci con l'URL della tua immagine
+    };
+  }
 });
+
+app.mount('#app');
